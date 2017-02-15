c4b892f283b6960f488eb99d60fa24b153ae23f5
Nickhil-Sethi <ns1635@nyu.edu>
2017-02-15 00:59:28 -0500

    more cleaning

--- tensorflow/tf_neural_network.py
+++ tensorflow/tf_neural_network.py
@@ -1,26 +1,16 @@
-'''
+'''Library of machine learning objects for neural networks'''
 
-Library of machine learning objects used to build neural networks
-
-'''
-
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 
 class linear_regression(object):
-
-	''''''''''
-	Linear Regression object
-	
-	'''''''''
-
 	def __init__(self,input,minibatch_size,n_in,n_out=1,init_noise=.01):
-		self.x = input
+		self.x 			= input
 		self.init_noise = init_noise
-		self.W = tf.Variable(tf.random_normal([n_in,n_out],stddev=self.init_noise), name='W')
-		self.b = tf.Variable(tf.random_normal([1,n_out],stddev=self.init_noise), name='b')
-		self.y_pred = tf.matmul(self.x, self.W) + self.b 
+		self.W 			= tf.Variable(tf.random_normal([n_in,n_out],stddev=self.init_noise), name='W')
+		self.b 			= tf.Variable(tf.random_normal([1,n_out],stddev=self.init_noise), name='b')
+		self.y_pred 	= tf.matmul(self.x, self.W) + self.b 
 
 
 	def set_parameters(self,W_new,b_new):
@@ -43,18 +33,7 @@ class linear_regression(object):
 		e = tf.sub(y_, self.prediction())
 		return tf.matmul( tf.transpose(e), e )
 
-
-
 class logistic_regression(object):
-
-
-	'''''''''''
-	Logistic_Regression class;
-
-	p(y|x) = C * sigm( W * x + b )
-	n_out x minibatch_size = C * sigm( n_out x n_in * n_in x minibatch_size + n x 1 )
-
-	'''''''''''
 
 	def __init__(self,input,n_in,n_out,v=.01):
 
@@ -65,9 +44,6 @@ class logistic_regression(object):
 		self.y_pred = tf.argmax(self.p_y_given_x, dimension=1)
 
 	def set_parameters(self,W_new,b_new):
-
-		# set parameters to new values 
-		# need to have this as a method, because computation graph uses functions
 
 		self.W.assign(W_new)
 		self.b.assign(b_new)
@@ -97,16 +73,6 @@ class logistic_regression(object):
 
 
 class hidden_layer(object):
-
-
-	'''''''''''
-	Hidden_Layer class;
-
-	output(x | W,b) = C * sigm( W * x + b )
-	n_out x minibatch_size = C * sigm( n_out x n_in * n_in x minibatch_size + n x 1 )
-
-	'''''''''''
-
 	def __init__(self,input, n_in, n_out, v=.01):
 		
 		self.x = input
@@ -137,13 +103,11 @@ class conv_layer(object):
 class neural_network(object):
 	def __init__(self,input,dimensions,init_noise=.01):
 
-		#layer = []
-
-		self.x = input
-		self.dimensions=dimensions
+		self.x 			= input
+		self.dimensions	= dimensions
 		self.num_layers = len(dimensions)-1
-		self.init_noise=init_noise
-		self.layer = list()
+		self.init_noise	= init_noise
+		self.layer 		= list()
 
 		for layer_index in range(self.num_layers-1):
 			if layer_index == 0:
